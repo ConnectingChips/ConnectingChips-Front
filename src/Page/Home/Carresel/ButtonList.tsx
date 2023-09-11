3d3fa0867dc3460af499b2d90d7619543e30348d
Kadesti <adesti3527@gmail.com>
2023-09-11 11:07:42 +0900

    🎨 Style : 캐러샐 이동량 / 버튼 스타일 수정

--- src/Page/Home/Carresel/ButtonList.tsx
+++ src/Page/Home/Carresel/ButtonList.tsx
@@ -41,7 +41,6 @@ const ButtonList = ({ buttonListProps }: { buttonListProps: ButtonListProps }): 
           <CarreselBtnList
             myCount={countList[index]}
             completedToday={doneList[index]}
-            uuid={uuidList[index]}
             key={index}
           />
         );
@@ -54,19 +53,19 @@ export default ButtonList;
 /** 2023-08-22 ButtonList.tsx - 캐러셀 버튼 영역 - Kadesti */
 const CarreselBtnList = ({
   myCount,
-  completedToday,
-  uuid,
+  completedToday
 }: {
   myCount: number;
   completedToday: boolean;
-  uuid: number;
 }) => {
+  const remind = async () => {
+    await fetch('/joined-minds/{mind_id}/remind', { method: 'PUT' });
+  };
+
   return (
     <>
       {myCount === 3 ? (
-        <Link to={`/groupPage/${uuid}`}>
-          <ClearBtnS>재작심하기</ClearBtnS>
-        </Link>
+        <ClearBtnS onClick={remind}>재작심하기</ClearBtnS>
       ) : completedToday ? (
         <TodayClearBtnS>오늘 작심 성공!</TodayClearBtnS>
       ) : (
@@ -79,7 +78,7 @@ const CarreselBtnList = ({
 };
 
 /** 2023-08-21 ButtonList.tsx - 다른 작심 둘러보기 버튼 */
-const ClearBtnS = styled.button`
+const TodayClearBtnS = styled.button`
   padding: 1rem;
   width: var(--width-my-mission);
   border-radius: 2rem;
@@ -88,7 +87,7 @@ const ClearBtnS = styled.button`
 `;
 
 /** 2023-08-27 ButtonList.tsx - 오늘 작심 성공! 버튼 */
-const TodayClearBtnS = styled.button`
+const ClearBtnS = styled.button`
   padding: 1rem;
   width: var(--width-my-mission);
   border-radius: 2rem;
--- src/StyleComp/ImageBoxS.tsx
+++ src/StyleComp/ImageBoxS.tsx
@@ -5,7 +5,7 @@ const ImageBoxS = styled.ul<{ count: number; sort: string; length: number }>`
   display: flex;
 
   transition: ${(props) => (props.sort === "next" ? (!props.count ? "" : "all 0.5s ease-in-out") : props.sort === "prev" ? (props.count === props.length ? "" : "all 0.5s ease-in-out") : "")};
-  transform: ${(props) => "translateX(-" + props.count * 190 + "px)"};
+  transform: ${(props) => "translateX(-" + props.count * 194 + "px)"};
   gap: 0.75rem;
   /* width: 100vw; */
 `;
