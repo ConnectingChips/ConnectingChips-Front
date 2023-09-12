03ad588cfe2e264d8077286ca2b875838cae2d6c
Kadesti <adesti3527@gmail.com>
2023-09-12 10:43:53 +0900

    🔨 Refactor : ArticleTab 공용 컴포넌트

--- src/Page/MyPage/MyPage.tsx
+++ src/Page/MyPage/MyPage.tsx
@@ -2,15 +2,17 @@ import styled from 'styled-components';
 import { Arrow_Left_B } from '../../Component/ArrowBarrel';
 import 기본프로필 from '../../image/예시사진모음/default_profile_W_MyPage.png';
 import infoIcon from '../../image/Icon/icon_Info.png';
-import MyPageArticle from './MyPageArticle';
+import ArticleTab from '../../Component/ArticleTab';
+import { CurrentMind, FinishedMind } from './MyPageMind';
 
 // FIXME: 버려질 코드
-import { myInfo } from '../../data/myInfo';
-import { useEffect, useState } from 'react';
+import { myInfo, myGroupList } from '../../data/myInfo';
 import { NavigateFunction, useNavigate } from 'react-router-dom';
 
 const MyPage = () => {
   const navigate = useNavigate();
+  const tabText = [`참여중인 작심${myGroupList.length}/3`, '참여했던 작심'];
+  const compArr = [<CurrentMind />, <FinishedMind />];
 
   return (
     <MyPageS>
@@ -27,7 +29,7 @@ const MyPage = () => {
         <img src={infoIcon} alt='기본프로필' />
         <p>최대 3개의 그룹까지 참여 가능합니다.</p>
       </LimitInfoS>
-      <MyPageArticle />
+      <ArticleTab tabText={tabText} compArr={compArr} />
       <MyPageSetS>
         <h2>설정</h2>
         <div onClick={() => logOutFetch(navigate)}>로그아웃</div>
