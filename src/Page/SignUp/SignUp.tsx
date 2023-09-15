89c23e89a5b65e394cf65a1370601bba5f723394
khkh0109 <ist200120@gmail.com>
2023-09-15 16:14:39 +0900

    ✨ Feat : 모든 입력폼, 약관 동의 체크되었을때 버튼 활성화, 필요없는 주석 제거

--- src/Page/SignUp/SignUp.tsx
+++ src/Page/SignUp/SignUp.tsx
@@ -40,43 +40,10 @@ const SignUp = (): JSX.Element => {
 
   useLoginCheck(navigate, 'Done');
 
-  // useEffect(() => {
-  // const isValidArr = [false, false, false, false, false];
-
-  // const idReg = /^(?!^\d+$)[a-zA-Z0-9]{2,10}$/;
-  // const passReg = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{10,20}$/;
-  // const emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[com]{3,}$/;
-  // const nicknameReg = /^[가-힣]{2,6}$/;
-
-  /** 2023-08-24 SignUp.tsx  조건 중 하나라도 안맞을시 false로 바꾸는 함수 */
-  // const setFalse = (index: number) => {
-  //   setIsValid(false);
-  //   isValidArr[index] = false;
-  // };
-
-  // id는 영문, 영문+숫자 중 1가지 2~10자 조합, 공백 불가
-  // if (idReg.test(id) && id.length <= 10) isValidArr[0] = true;
-  // else return setFalse(0);
-
-  // pw는 영문+숫자 10~20자 조합, 공백 불가
-  // if (passReg.test(password) === true) isValidArr[1] = true;
-  // else return setFalse(1);
-
-  // pw는 pwconfirm과 동일해야함
-  // if (password === confirmPassword) isValidArr[2] = true;
-  // else return setFalse(2);
-
-  // email은 @, .com 포함
-  // if (emailReg.test(email) === true) isValidArr[3] = true;
-  // else return setFalse(3);
-
-  // nickname은 한글 2-6자
-  // if (nicknameReg.test(nickname) === true) isValidArr[4] = true;
-  // else return setFalse(4);
-
-  // const findFalse = isValidArr.find((isvalid) => isvalid === false);
-  // if (findFalse === undefined) setIsValid(true);
-  // }, [id, password, confirmPassword, email, nickname]);
+  useEffect(() => {
+    const isAllChecked = Object.values(validation).every((value) => value);
+    setIsValid(isAllChecked);
+  }, [validation]);
 
   const idValidationCheck = () => {
     const idReg = /^(?!^\d+$)[a-zA-Z0-9]{2,10}$/g;
@@ -108,27 +75,15 @@ const SignUp = (): JSX.Element => {
     setValidation((prev) => ({ ...prev, confirmPassword: isSamePassword }));
   };
 
-  /** 2023-08-24 SignUp.tsx - 로그인 요청 핸들러 */
-  const SignupSubmit = async (e: React.MouseEvent<HTMLFormElement, MouseEvent>): Promise<void> => {
-    e.preventDefault();
-    try {
-      // await axios.post("/users/sign-up", {
-      //   nickname,
-      //   password,
-      //   confirmPassword,
-      // });
-      navigate('/LogIn');
-    } catch (error) {
-      console.error('회원가입 오류: ', error);
-      setInputState('failed');
-    }
+  const handleSubmitButtonClick = () => {
+    navigate('/LogIn');
   };
 
   return (
     <LogInS>
       <Loginheader type='회원가입' />
       <Banner />
-      <LoginFormS onSubmit={SignupSubmit}>
+      <LoginFormS>
         <LoginInputContainerS>
           <h2>아이디</h2>
           <SignUpInput
@@ -196,7 +151,7 @@ const SignUp = (): JSX.Element => {
       <Terms isAllAgreed={isAllAgreed} setIsAllAgreed={setIsAllAgreed} />
       <BtnWrapperS>
         {isValid && isAllAgreed ? (
-          <SignClearBtnS type='submit' className='btn_width'>
+          <SignClearBtnS type='submit' className='btn_width' onClick={handleSubmitButtonClick}>
             <p>회원가입</p>
           </SignClearBtnS>
         ) : (
