c96ff3add392d631fcbb9c134d031eafe72f4a76
Kadesti <adesti3527@gmail.com>
2023-09-15 21:03:24 +0900

    ✨ Feat : GA 코드 작성

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,29 @@
-import ReactDOM from "react-dom/client";
-import MobileBGS from "./StyleComp/MobileBGS";
-import App from "./App";
-import GlobalStyle from "./StyleComp/GlobalStyle";
-import reportWebVitals from "./reportWebVitals";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import MobileBGS from './StyleComp/MobileBGS';
+import App from './App';
+import GlobalStyle from './StyleComp/GlobalStyle';
+import reportWebVitals from './reportWebVitals';
 // import axios from "axios";
+import ReactGA from 'react-ga4';
+
 
 // axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
+  ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   // <React.StrictMode>
-  <MobileBGS>
-    <GlobalStyle />
-    <App />
-  </MobileBGS>
+  <BrowserRouter>
+    <MobileBGS>
+      <GlobalStyle />
+      <App />
+    </MobileBGS>
+  </BrowserRouter>,
+
   // </React.StrictMode>
 );
 
@@ -21,4 +31,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
