d9e0dffc6ec077c41aeb82ac43beba693a8bc130
khkh0109 <ist200120@gmail.com>
2023-09-17 12:16:36 +0900

    ✨ Feat : 로그인 요청 함수 추가

---
+++ src/API/login.ts
@@ -0,0 +1,17 @@
+import { postData } from './axiosConfig';
+
+/**
+ * 로그인 요청 보내는 함수
+ * 필요: 아이디, 비밀번호
+ * 성공: 200
+ * 실패: 아이디/비번 다른 경우
+ */
+interface Login {
+  accessToken: string;
+}
+
+export const postLogin = async (id: string, password: string): Promise<Login> => {
+  const loginData = { id, password };
+  const response = await postData<Login>('/login', loginData);
+  return response.result;
+};
