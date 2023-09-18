7b9197e9f619034b7433c37ca02926444042aa6a
pizzaYami <cyj9407@naver.com>
2023-09-18 17:38:07 +0900

    🔨 Refactor : GroupArticle변경

--- src/Page/GroupPage/GroupPage.tsx
+++ src/Page/GroupPage/GroupPage.tsx
@@ -11,8 +11,6 @@ import {
 } from './GroupPageBarrel';
 import { GroupPostList } from './GroupPostList';
 
-// TODO: GroupPost 하나도 없다면 noMind이미지 뜨게하기
-// TODO: GroupPost 2개씩 넣으면 이미지 두개들어가는거 고치기
 /** 2023-08-22 GroupPage.tsx - 메인 컴프 */
 const GroupPage = (): JSX.Element => {
   const { intro, rule, url } = useFindGroup('page');
@@ -22,29 +20,14 @@ const GroupPage = (): JSX.Element => {
       <GroupHeader />
       {/* FIXME: url 안먹힘 */}
       <GroupImageS url={url} />
-      <GroupSummary intro={intro} rule={rule} selected={[0, 1, 3]} />
+      <GroupArticle groupText={intro} groupRule={rule} selected={[0, 1, 3]} passsort='Page' />
+      <DivideBaS />
       <GroupPostList />
     </GroupPageS>
   );
 };
 
 export default GroupPage;
-
-type GroupPostProps = {
-  intro: string;
-  rule: string;
-  selected: number[] | null;
-};
-
-/** 2023-08-26 GroupPage.tsx - 그룹페이지 소개글 - 0 : 헤드라인 1 : 소개 2 : 규칙 3 : 버튼 */
-const GroupSummary = ({ intro, rule, selected }: GroupPostProps) => {
-  return (
-    <>
-      <GroupArticle groupText={intro} groupRule={rule} selected={selected} passsort='Page' />
-      <DivideBaS />
-    </>
-  );
-};
 
 const GroupPageS = styled.div`
   width: var(--width-mobile);
