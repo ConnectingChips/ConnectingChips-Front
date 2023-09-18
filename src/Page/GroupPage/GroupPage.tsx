bb6081aac7ba7aee187c4cbf2c404c2e0d082ece
pizzaYami <cyj9407@naver.com>
2023-09-18 17:34:07 +0900

    🔨 Refactor : GroupNoPost 생성

--- src/Page/GroupPage/GroupPage.tsx
+++ src/Page/GroupPage/GroupPage.tsx
@@ -2,13 +2,14 @@ import { useState, styled } from './GroupPageBarrel';
 import {
   useFindGroup,
   GroupHeader,
-  type LikeBind,
-  Comment,
+  // type LikeBind,
+  // Comment,
   DivideBaS,
-  GroupActive,
+  // GroupActive,
   // Mind,
   GroupArticle,
 } from './GroupPageBarrel';
+import { GroupPostList } from './GroupPostList';
 
 // TODO: GroupPost 하나도 없다면 noMind이미지 뜨게하기
 // TODO: GroupPost 2개씩 넣으면 이미지 두개들어가는거 고치기
@@ -45,33 +46,6 @@ const GroupSummary = ({ intro, rule, selected }: GroupPostProps) => {
   );
 };
 
-/** 2023-08-26 GroupPage.tsx - 그룹페이지 글 항목 */
-const GroupPostList = () => {
-  // TODO: post업애려면 Commendted false로 바꾸기
-  const [Commented, setCommented] = useState(true);
-  const [isLiked, setIsLiked] = useState(false);
-  const likeBind: LikeBind = { isLiked, setIsLiked };
-
-  return (
-    <GroupPostS>
-      <h2>작심 인증글</h2>
-      {Commented ? (
-        <>
-          <GroupActive passsort='Page' setCommented={setCommented} likeBind={likeBind} />
-          <Comment Commented={Commented} />
-        </>
-      ) : (
-        <GroupNoMindS>
-          {/* TODO: 이미지 다른곳에 저장하기 */}
-          <img src={`${process.env.PUBLIC_URL}/noMind.png`} alt='noMind'></img>
-          <h2>등록된 인증글이 없습니다.</h2>
-          <p>가장 먼저 작심을 인증해 보세요!</p>
-        </GroupNoMindS>
-      )}
-    </GroupPostS>
-  );
-};
-
 const GroupPageS = styled.div`
   width: var(--width-mobile);
   position: relative;
@@ -83,31 +57,4 @@ const GroupImageS = styled.div<{ url: string }>`
   background-size: 25rem;
   background-position: 0 -1rem;
   height: 10rem;
-`;
-
-const GroupPostS = styled.div`
-  display: flex;
-  flex-direction: column;
-  border-radius: 1rem;
-  gap: 0.75rem;
-  // FIXME: commentinput의 하단자리가 부족해서 댓글을 가려버려서 임시로 넣음
-  margin-bottom: 50px;
-  h2 {
-    font-size: 1rem;
-  }
-`;
-
-/** 2023-09-12 GroupPage.tsx - 그룹페이지 글 없을 때 사진 */
-const GroupNoMindS = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 0.5rem;
-  img {
-    width: 7.5rem;
-    margin-bottom: 1rem;
-  }
-  p {
-    color: var(--font-color3);
-  }
 `;
---
+++ src/Page/GroupPage/GroupPostList.tsx
@@ -0,0 +1,63 @@
+/** 2023-08-26 GroupPage.tsx - 그룹페이지 글 항목 */
+import { useState, styled } from './GroupPageBarrel';
+import { type LikeBind, Comment, GroupActive } from './GroupPageBarrel';
+
+const GroupPostList = () => {
+  // TODO: post업애려면 Commendted false로 바꾸기
+  const [Commented, setCommented] = useState(true);
+  const [isLiked, setIsLiked] = useState(false);
+  const likeBind: LikeBind = { isLiked, setIsLiked };
+
+  return (
+    <GroupPostListS>
+      <h2>작심 인증글</h2>
+      {Commented ? (
+        <>
+          <GroupActive passsort='Page' setCommented={setCommented} likeBind={likeBind} />
+          <Comment Commented={Commented} />
+        </>
+      ) : (
+        <GroupNoPost />
+      )}
+    </GroupPostListS>
+  );
+};
+
+export { GroupPostList };
+
+const GroupNoPost = () => {
+  return (
+    <GroupNoPostS>
+      <img src={`${process.env.PUBLIC_URL}/noMind.png`} alt='noMind'></img>
+      <h2>등록된 인증글이 없습니다.</h2>
+      <p>가장 먼저 작심을 인증해 보세요!</p>
+    </GroupNoPostS>
+  );
+};
+
+const GroupPostListS = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 1rem;
+  gap: 0.75rem;
+  // FIXME: commentinput의 하단자리가 부족해서 댓글을 가려버려서 임시로 넣음
+  margin-bottom: 50px;
+  h2 {
+    font-size: 1rem;
+  }
+`;
+
+/** 2023-09-12 GroupPage.tsx - 그룹페이지 글 없을 때 사진 */
+const GroupNoPostS = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  img {
+    width: 7.5rem;
+    margin-bottom: 1rem;
+  }
+  p {
+    color: var(--font-color3);
+  }
+`;
