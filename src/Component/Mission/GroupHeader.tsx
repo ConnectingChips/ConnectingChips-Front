a27723dc3d46df2efde5dd2db04455fde78ddb8f
pizzaYami <cyj9407@naver.com>
2023-09-22 13:20:46 +0900

    ⚠ Fix : 인증하거나 재작심하기안하면 header에 upload링크 사라짐

--- src/Component/Mission/GroupHeader.tsx
+++ src/Component/Mission/GroupHeader.tsx
@@ -21,16 +21,18 @@ const GroupHeader = ({ children, className }: GroupHeaderProps): JSX.Element => 
   const isUpload = path.indexOf('/upload') !== -1;
   const { mindId } = useParams();
   const [isDoneToday, setIsDoneToday] = useState<boolean>(false);
+  const [keepJoin, setKeepJoin] = useState<boolean>(false);
   useEffect(() => {
     getkeepJoin(Number(mindId)).then((data) => {
       setIsDoneToday(data.isDoneToday);
+      setKeepJoin(data.keepJoin);
     });
   }, []);
 
   return (
     <GroupBGHeaderS className={className}>
       <img src={Arrow_Left_B} onClick={goBack} alt='Arrow icon' />
-      {(!isUpload || !isDoneToday) && (
+      {!(isUpload || isDoneToday || keepJoin) && (
         <Link to={`/uploadPost/${mindId}`}>
           <img src={post_Icon} alt='post icon' />
         </Link>
