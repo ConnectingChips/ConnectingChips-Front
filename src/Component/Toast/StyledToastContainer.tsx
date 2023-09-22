4af7448c87f7b3c344cf49c6bb723071ff948d47
khkh0109 <ist200120@gmail.com>
2023-09-23 01:36:14 +0900

    ✨ Feat : 이미지 파일 10mb 초과 시 토스트 메시지 보여주기

---
+++ src/Component/Toast/StyledToastContainer.tsx
@@ -0,0 +1,80 @@
+import styled from 'styled-components';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const StyledToastContainer = (): JSX.Element => {
+  return (
+    <StyledContainer
+      position='bottom-center'
+      hideProgressBar={true}
+      closeButton={false}
+      autoClose={3000}
+      limit={2}
+    />
+  );
+};
+
+const StyledContainer = styled(ToastContainer)`
+  display: flex;
+  justify-content: center;
+
+  &&.Toastify__toast-container {
+    padding-bottom: 88px; // 버튼 배경 박스에 딱 붙게
+    font-size: 12px;
+  }
+
+  .Toastify__toast-body {
+    padding: 0;
+    margin: 0;
+  }
+
+  .Toastify__toast {
+    min-height: 0;
+    padding: 13px 16px;
+  }
+
+  .net-error,
+  .client-error,
+  .img-limit-err {
+    border-radius: 0.625rem;
+    color: white;
+    font-size: var(--body-c);
+    font-weight: 400;
+    font-family: Noto Sans KR;
+    background-color: rgba(0, 0, 0, 0.6);
+  }
+
+  .net-error,
+  .client-error {
+    width: 23rem;
+  }
+
+  .img-limit-err {
+    min-width: 17.5625rem;
+  }
+
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  @keyframes fade-out {
+    0% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+
+  .fade-in {
+    animation: fade-in 0.5s ease-in-out both;
+  }
+  .fade-out {
+    animation: fade-out 0.5s ease-in-out both;
+  }
+`;
