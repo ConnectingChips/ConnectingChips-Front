a9b33fac7c525281d327b7f944827ef8a40a04e4
pizzaYami <cyj9407@naver.com>
2023-09-22 13:47:09 +0900

    ✨ Feat : GroupPost image가 없다면 안띄우게 하기

--- src/Page/GroupPage/GroupPost.tsx
+++ src/Page/GroupPage/GroupPost.tsx
@@ -29,13 +29,17 @@ const GroupPost = ({
     setEdit,
   };
 
+  console.log(postData);
+
   return (
     <GroupPostS passsort={passsort}>
       <PostS>
         <PostHeader editbind={editbind} postData={postData} refreshBind={refreshBind} />
-        <PostImageS>
-          <img src={postData.image} alt='업로드 사진' />
-        </PostImageS>
+        {postData.image !== '' && (
+          <PostImageS>
+            <img src={postData.image} alt='업로드 사진' />
+          </PostImageS>
+        )}
         <PostContent
           editbind={editbind}
           postData={postData}
