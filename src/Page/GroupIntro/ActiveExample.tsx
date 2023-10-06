5f9be0c906b8962cd6bcde93cd9ed38103ed2675
pizzaYami <cyj9407@naver.com>
2023-10-06 10:14:06 +0900

    JSSC-66  🔨 Refactor : IntroExample 삭제

--- src/Page/GroupIntro/ActiveExample.tsx
+++ src/Page/GroupIntro/ActiveExample.tsx
@@ -2,22 +2,9 @@ import { styled } from 'styled-components';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { PageSort } from '../../Type/Mind';
-import 자전거운동 from '../../image/예시사진모음/자전거운동.jpg';
 import defaultImage from '../../image/ex_image_err.png';
 import { Arrow_Down, Arrow_Up } from '../../Component/ArrowBarrel';
 import { getExampleImage } from '../../API/Mind';
-
-/** 2023-08-22 ActiveExample.tsx - 작심 인증 글 */
-const IntroExample = ({ passsort }: { passsort: PageSort }): JSX.Element => {
-  return (
-    <PostS passsort={passsort}>
-      <h2>인증 사진 예시</h2>
-      <PostImageS>
-        <img src={자전거운동} alt='업로드 사진' />
-      </PostImageS>
-    </PostS>
-  );
-};
 
 /** 2023-08-22 ActiveExample.tsx - 작심 인증 글 */
 const CreateExample = (): JSX.Element => {
@@ -57,7 +44,7 @@ const CreateExample = (): JSX.Element => {
   );
 };
 
-export { IntroExample, CreateExample };
+export { CreateExample };
 
 /** 2023-08-22 ActiveExample.tsx - 그룹페이지 아티클 */
 const PostS = styled.article<{ passsort: PageSort }>`
@@ -67,7 +54,6 @@ const PostS = styled.article<{ passsort: PageSort }>`
   color: var(--font-color1);
   padding: 1rem;
   border-radius: 1rem;
-
   display: flex;
   flex-direction: column;
   gap: var(--height-gap);
@@ -76,7 +62,6 @@ const PostS = styled.article<{ passsort: PageSort }>`
 const CreateHeaderS = styled.div`
   display: flex;
   justify-content: space-between;
-
   button {
     display: flex;
     align-items: center;
@@ -86,11 +71,9 @@ const CreateHeaderS = styled.div`
 /** 2023-08-22 ActiveExample.tsx - 그룹페이지 아티클 인증 이미지(임시) */
 const PostImageS = styled.div`
   overflow: hidden;
-
   display: flex;
   justify-content: center;
   align-items: center;
-
   img {
     width: 100%;
   }
