346b658ff01502207c2f809042f120ec8cc2d6c0
pizzaYami <cyj9407@naver.com>
2023-10-07 20:34:51 +0900

    JSSC-66  🔨 ⏪️ Rename : CommentToolbarS

--- src/Page/GroupPage/Comment/CommentToolbar.tsx
+++ src/Page/GroupPage/Comment/CommentToolbar.tsx
@@ -5,29 +5,29 @@ import {
 } from '../../../Component/ArrowBarrel';
 import { BoardsType } from '../GroupPageBarrel';
 import Bind from '../../../Type/Bind';
-interface CommentHeaderProps {
+interface CommentToolbarProps {
   commentFlipBind: Bind<boolean>;
   postData: BoardsType;
 }
 
-const CommentToolbar = ({ commentFlipBind, postData }: CommentHeaderProps) => {
+const CommentToolbar = ({ commentFlipBind, postData }: CommentToolbarProps) => {
   const { state: commentFlip, Setter: setCommentFlip } = commentFlipBind;
   return (
-    <CommentHeaderS>
+    <CommentToolbarS>
       <h2 className='commentfont'>댓글 {postData.commentCount}</h2>
       <img
         src={commentFlip ? Arrow_icon_Down : Arrow_icon_Up}
         alt='댓글열기'
         onClick={() => setCommentFlip(!commentFlip)}
       />
-    </CommentHeaderS>
+    </CommentToolbarS>
   );
 };
 
 // TOFIX: export defalut 왜 안돼
 export { CommentToolbar };
 
-const CommentHeaderS = styled.div`
+const CommentToolbarS = styled.div`
   display: flex;
   gap: 0.5rem;
   .commentfont {
