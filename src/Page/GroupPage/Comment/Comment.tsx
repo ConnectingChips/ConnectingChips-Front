578bcfdebde70ee568a5d083299a326c87b8ef12
pizzaYami <cyj9407@naver.com>
2023-10-08 01:57:58 +0900

    JSSC-66  ⏪️ Rename : ReplyBoxMaker 삭제하고 CommentBox로 병합

--- src/Page/GroupPage/Comment/Comment.tsx
+++ src/Page/GroupPage/Comment/Comment.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
-import { useState } from 'react';
+import { SetStateAction, useEffect, useState } from 'react';
 import { CommentType, ReplyType } from '../../../API/Boards';
 import { GetUser } from '../GroupPageBarrel';
-import { deleteComment, deleteReply } from '../../../API/Comment';
+import { deleteComment } from '../../../API/Comment';
 import DeleteModal from '../../../Component/DeleteModal';
 import { useRecoilState } from 'recoil';
 import { refreshState } from '../../../data/initialData';
+
 interface CommentProps {
   commentData: CommentType;
   userInfo: GetUser;
@@ -15,7 +16,7 @@ interface CommentProps {
 
 /** 댓글 box */
 const Comment = ({ commentData, userInfo, setInputToggle, setIsComment }: CommentProps) => {
-  const { profileImage, commentId } = commentData;
+  const { commentId } = commentData;
   const [modalBtn, setModalBtn] = useState(false);
   const modalBind = { state: modalBtn, Setter: setModalBtn };
   const [refresh, setRefresh] = useRecoilState<number>(refreshState);
@@ -27,42 +28,25 @@ const Comment = ({ commentData, userInfo, setInputToggle, setIsComment }: Commen
     });
   };
 
-  const CommentBox = () => {
-    const { nickname, createDate, content, userId } = commentData;
-    const openModal = () => setModalBtn(true);
-
-    const addReplyHandler = () => {
-      setInputToggle(false);
-      setIsComment(commentId);
-    };
-
-    return (
-      <CommentBoxContainerS>
-        <CommentBoxS>
-          <img src={profileImage} alt='댓글프로필' />
-          <div className='profile'>
-            <h2>{nickname}</h2>
-            <p className='date'>{createDate}</p>
-            <p className='text'>{content}</p>
-          </div>
-        </CommentBoxS>
-        <CommentBoxOptionS>
-          <p onClick={addReplyHandler}>답글</p>
-          {userInfo.userId === userId && (
-            <p onClick={openModal} className={'delete'}>
-              삭제
-            </p>
-          )}
-        </CommentBoxOptionS>
-      </CommentBoxContainerS>
-    );
-  };
-
   return (
     <CommentContainerS>
-      <CommentBox />
-      {commentData.replyList.map((replyData, i) => {
-        return <ReplyBoxMaker replyData={replyData} userInfo={userInfo} key={i} />;
+      <CommentBox
+        CommentData={commentData}
+        userInfo={userInfo}
+        setInputToggle={setInputToggle}
+        setIsComment={setIsComment}
+        setModalBtn={setModalBtn}
+      />
+      {commentData.replyList.map((replyData) => {
+        return (
+          <CommentBox
+            replyData={replyData}
+            userInfo={userInfo}
+            setInputToggle={setInputToggle}
+            setIsComment={setIsComment}
+            setModalBtn={setModalBtn}
+          />
+        );
       })}
       <DeleteModal modalBind={modalBind} deleteAction={deleteCommentHandler} />
     </CommentContainerS>
@@ -71,84 +55,111 @@ const Comment = ({ commentData, userInfo, setInputToggle, setIsComment }: Commen
 
 export default Comment;
 
-interface ReplyBoxMakerProps {
-  replyData: ReplyType;
+interface CommentBoxProps {
+  CommentData?: CommentType;
+  replyData?: ReplyType;
   userInfo: GetUser;
+  setInputToggle: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsComment: React.Dispatch<React.SetStateAction<number>>;
+  setModalBtn: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-/** 답글 box */
-const ReplyBoxMaker = ({ replyData, userInfo }: ReplyBoxMakerProps) => {
-  const { profileImage, replyId } = replyData;
-  const [modalBtn, setModalBtn] = useState(false);
-  const modalBind = { state: modalBtn, Setter: setModalBtn };
-  const [refresh, setRefresh] = useRecoilState<number>(refreshState);
+const CommentBox = ({
+  CommentData,
+  replyData,
+  userInfo,
+  setInputToggle,
+  setIsComment,
+  setModalBtn,
+}: CommentBoxProps) => {
+  const openModal = () => setModalBtn(true);
+  const [isReply, setIsReply] = useState<boolean>(false);
 
-  const deleteReplyHandler = async () => {
-    await deleteReply(replyId).then(() => {
-      setRefresh(refresh + 1);
-    });
-  };
+  let nickname;
+  let createDate;
+  let content;
+  let userId;
+  let profileImage;
+  let commentId: SetStateAction<number>;
+  let replyId: SetStateAction<number>;
 
-  const ReplyContent = (): JSX.Element => {
-    const { nickname, createDate, content, userId } = replyData;
-    const openModal = () => setModalBtn(true);
-    const WriterInfo = () => {
-      return (
-        <div>
-          <div className='profile'>
-            <h2>{nickname}</h2>
-            <p className='date'>{createDate}</p>
-          </div>
-          <p className='text'>{content}</p>
-        </div>
-      );
-    };
+  // const [commentId, setCommentId] = useState<number>();
+  // const [refresh, setRefresh] = useRecoilState<number>(refreshState);
+  useEffect(() => {
+    if (CommentData) {
+      setIsReply(false);
+    }
+    if (replyData) {
+      setIsReply(true);
+    }
+  }, [CommentData, replyData]);
 
-    return (
-      <ReplyContentS>
-        <WriterInfo />
-        {userInfo.userId === userId ? (
-          <CommentBoxOptionS>
-            <p onClick={openModal} className='delete'>
-              삭제
-            </p>
-          </CommentBoxOptionS>
-        ) : (
-          <></>
-        )}
-      </ReplyContentS>
-    );
+  if (CommentData) {
+    nickname = CommentData.nickname;
+    createDate = CommentData.createDate;
+    content = CommentData.content;
+    userId = CommentData.userId;
+    profileImage = CommentData.profileImage;
+    commentId = CommentData.commentId;
+  } else if (replyData) {
+    nickname = replyData.nickname;
+    createDate = replyData.createDate;
+    content = replyData.content;
+    userId = replyData.userId;
+    profileImage = replyData.profileImage;
+    replyId = replyData.replyId;
+  }
+
+  const addReplyHandler = () => {
+    setInputToggle(false);
+    setIsComment(commentId);
   };
 
   return (
-    <ReplyContainerS>
-      <img src={profileImage} alt='답글프로필' />
-      <ReplyContent />
-      <DeleteModal modalBind={modalBind} deleteAction={deleteReplyHandler} />
-    </ReplyContainerS>
+    <CommentBoxContainerS isReply={isReply}>
+      <CommentBoxS>
+        <CommentBoxProfileImgS src={profileImage} alt='댓글프로필' />
+        <div>
+          <CommentBoxProfile>
+            <h2>{nickname}</h2>
+            <p className='date'>{createDate}</p>
+          </CommentBoxProfile>
+          <CommentBoxContent>{content}</CommentBoxContent>
+          <CommentBoxOptionS>
+            {isReply ? null : <p onClick={addReplyHandler}>답글</p>}
+            {userInfo.userId === userId && (
+              <p onClick={openModal} className={'delete'}>
+                삭제
+              </p>
+            )}
+          </CommentBoxOptionS>
+        </div>
+      </CommentBoxS>
+    </CommentBoxContainerS>
   );
 };
 
 const CommentBoxS = styled.div`
   display: flex;
+  gap: 0.5rem;
+`;
 
-  .profile {
-    display: flex;
-    flex-de
-    align-items: center;
-    gap: var(--height-gap);
-    margin-bottom: 0.37rem;
+const CommentBoxProfileImgS = styled.img`
+  width: 1.875rem;
+  height: 1.875rem;
+`;
+
+const CommentBoxProfile = styled.div`
+  display: flex;
+  align-items: center;
+  gap: var(--height-gap);
+
+  h2 {
+    font-size: 0.875rem;
   }
-
-  .date {
+  p {
     font-size: 0.75rem;
     color: var(--font-color3);
-  }
-
-  .text {
-    font-size: 0.875rem;
-    color: var(--font-color2);
-    margin-bottom: var(--height-gap);
   }
 `;
 
@@ -158,27 +169,28 @@ const CommentContainerS = styled.div`
   min-height: 2rem;
   border-radius: 1rem;
   flex-direction: column;
+  gap: 0.5rem;
   img {
     width: 1.875rem;
   }
 `;
 
-const ReplyContainerS = styled(CommentContainerS)`
-  border: 2px solid var(--color-line);
-  padding: 1rem;
+const CommentBoxContent = styled.p`
+  font-size: 0.875rem;
+  color: var(--font-color2);
+  margin-bottom: var(--height-gap);
 `;
 
-const CommentBoxContainerS = styled.div`
-  margin-left: 0.5rem;
+const CommentBoxContainerS = styled.div<{ isReply: boolean }>`
+  ${(props) =>
+    props.isReply
+      ? 'background-color : var(--color-bg);padding: 1rem;margin-left: 2.375rem;border-radius: 1rem;'
+      : null};
 
   width: 19.0625rem;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-`;
-
-const ReplyContentS = styled(CommentBoxContainerS)`
-  width: 18.0625rem;
 `;
 
 const CommentBoxOptionS = styled.div`
