38c8154572d718ba0fbf557d7797676b81970aaa
pizzaYami <cyj9407@naver.com>
2023-10-14 17:07:26 +0900

    [JSSC-156]  🗑️ Remove: 더미데이터 제거

--- src/Page/Home/Carresel/CarreselBarrel.ts
+++ src/Page/Home/Carresel/CarreselBarrel.ts
@@ -2,9 +2,6 @@ import { styled } from 'styled-components';
 import { Link } from 'react-router-dom';
 import { useEffect, useRef, useState } from 'react';
 
-import co_bike from '../../../image/커버 이미지/co_bike_work/co_bike_work_main.jpg';
-import co_fit from '../../../image/커버 이미지/co_fit_enter/co_fit_enter_main.jpg';
-import co_walk from '../../../image/커버 이미지/co_walk_stroll/co_walk_stroll_main.jpg';
 import ArrowLeft from '../../../image/Icon/Arrow/Arrow_icon_W.svg';
 import Arrow_Right from '../../../image/Icon/Arrow/Arrow_icon_Right_W.png';
 
@@ -16,7 +13,6 @@ import { Mylist } from '../../../Type/Mind';
 
 export { ChipList, MissionSingleWideS };
 export { styled, Link, useEffect, useRef, useState };
-export { co_bike, co_fit, co_walk };
 export { ArrowLeft, Arrow_Right };
 export { chip_Active, chip_NoneActive };
 export type { Mylist };
