2cc604723cd3efe4e512877c03b097be6803fd0a
pizzaYami <cyj9407@naver.com>
2023-10-16 12:52:00 +0900

    [JSSC-4]  🔨 Refactor: CommentToolbar div > h2 대신에 h2하나만 사용하도록

--- src/Page/GroupPage/Comment/CommentToolbar.tsx
+++ src/Page/GroupPage/Comment/CommentToolbar.tsx
@@ -1,29 +1,18 @@
 import styled from 'styled-components';
-import {
-  Arrow_Up as Arrow_icon_Down,
-  Arrow_Down as Arrow_icon_Up,
-} from '../../../Component/ArrowBarrel';
 import { BoardsType } from '../GroupPageBarrel';
-import Bind from '../../../Type/Bind';
+
 interface CommentToolbarProps {
   postData: BoardsType;
 }
 
 const CommentToolbar = ({ postData }: CommentToolbarProps) => {
-  return (
-    <CommentToolbarS>
-      <h2 className='commentfont'>댓글 {postData.commentCount}</h2>
-    </CommentToolbarS>
-  );
+  return <CommentToolbarS>댓글 {postData.commentCount}</CommentToolbarS>;
 };
 
 // TOFIX: export defalut 왜 안돼
 export { CommentToolbar };
 
-const CommentToolbarS = styled.div`
-  display: flex;
-  gap: 0.5rem;
-  .commentfont {
-    font-size: 1rem;
-  }
+const CommentToolbarS = styled.h2`
+  margin-bottom: 0.75rem;
+  font-size: 1rem;
 `;
