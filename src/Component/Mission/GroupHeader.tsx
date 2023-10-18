efc261bba8ac23f02fbbb663caa365a9e018e14f
pizzaYami <cyj9407@naver.com>
2023-10-18 09:40:25 +0900

    [JSSC-178]  ✨ Feat: groupHeader 공통 컴포넌트 만들기

--- src/Component/Mission/GroupHeader.tsx
+++ src/Component/Mission/GroupHeader.tsx
@@ -1,20 +1,34 @@
-import { styled } from 'styled-components';
 import { useEffect, useState } from 'react';
-import { Arrow_Left_B, Arrow_Left_W } from '../ArrowBarrel';
-import post_Icon from '../../image/Icon/post_Icon.svg';
-import post_Icon_locked from '../../image/Icon/post_Icon_locked.svg';
-import { Link, useLocation, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { styled } from 'styled-components';
 import { getkeepJoin } from '../../API/Mind';
 import { useRecoilState } from 'recoil';
 import { refreshState } from '../../data/initialData';
+import { Arrow_Left_B, Arrow_Left_W } from '../ArrowBarrel';
+import post_Icon from '../../image/Icon/post_Icon.svg';
+import post_Icon_locked from '../../image/Icon/post_Icon_locked.svg';
 
-/** 2023-08-25 GroupHeader.tsx - 그룹 페이지 헤더 */
-const GroupHeader = (): JSX.Element => {
-  const path = useLocation().pathname;
-  const isUpload = path.indexOf('/upload') !== -1;
+interface GroupHeaderType {
+  BGcolor?: string;
+  upload?: boolean;
+  backBtnColor?: 'black' | 'white';
+  text?: string;
+}
+// 그룹페이지 헤더
+// 배경색 : blur or color
+// 글쓰기 아이콘 유무
+// 뒤로가기 버튼 white or black
+// 중앙 텍스트
+const GroupHeader = ({ BGcolor, upload, backBtnColor, text }: GroupHeaderType): JSX.Element => {
   const { mindId } = useParams();
   const [isDoneToday, setIsDoneToday] = useState<boolean>(false);
   const [refresh] = useRecoilState<number>(refreshState);
+
+  if (!BGcolor) {
+    BGcolor = 'white';
+  } else if (BGcolor === 'blur') {
+    BGcolor = '';
+  }
 
   useEffect(() => {
     getkeepJoin(Number(mindId)).then((data) => {
@@ -23,9 +37,7 @@ const GroupHeader = (): JSX.Element => {
   }, [refresh, mindId]);
 
   const UploadIcon = (): JSX.Element => {
-    return isUpload ? (
-      <></>
-    ) : !isDoneToday ? (
+    return !isDoneToday ? (
       <Link to={`/uploadPost/${mindId}`}>
         <img src={post_Icon} alt='post icon' />
       </Link>
@@ -34,44 +46,42 @@ const GroupHeader = (): JSX.Element => {
     );
   };
 
+  const goBack = (): void => window.history.back();
+  const BackIcon = () => (
+    <img
+      src={backBtnColor === 'white' ? Arrow_Left_W : Arrow_Left_B}
+      onClick={goBack}
+      alt='Arrow icon'
+    />
+  );
   return (
-    <GroupHeaderContainerS>
+    <GroupHeaderContainerS BGcolor={BGcolor}>
       <BackIcon />
-      <UploadIcon />
+      <p className='title'>{text}</p>
+      {upload ? <UploadIcon /> : <div></div>}
     </GroupHeaderContainerS>
   );
 };
 
-const GroupIntroHeader = (): JSX.Element => {
-  return (
-    <GroupHeaderS onClick={goBack}>
-      <img src={Arrow_Left_W} alt='Arrow icon' />
-    </GroupHeaderS>
-  );
-};
-
 const goBack = (): void => window.history.back();
-const BackIcon = () => <img src={Arrow_Left_B} onClick={goBack} alt='Arrow icon' />;
 
-export { GroupHeader, GroupIntroHeader, goBack, BackIcon };
+export { GroupHeader, goBack };
 
-/** 2023-08-22 GroupHeader.tsx - 그룹 인트로 뒤로가기 */
-export const GroupHeaderS = styled.header`
+export const GroupHeaderContainerS = styled.header<{ BGcolor: string }>`
   position: fixed;
   display: flex;
   align-items: center;
   padding: 1rem;
   top: 0;
+  left: 0;
   box-sizing: border-box;
   height: var(--height-header);
-`;
-
-/** 2023-08-22 GroupHeader.tsx - 그룹페이지 상단 고정 */
-export const GroupHeaderContainerS = styled(GroupHeaderS)`
   width: 100vw;
-  height: var(--height-header);
   justify-content: space-between;
-  background-color: white;
   z-index: 100;
-  top: 0;
+  background-color: ${(props) => (props.BGcolor ? props.BGcolor : '')};
+  .title {
+    font-size: 1.25rem;
+    font-weight: 500;
+  }
 `;
